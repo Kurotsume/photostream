310ed7de5d5d5807070252e594f6a7c2b760479f
Kurotsume <boweezy777@gmail.com>
2017-01-22 19:32:48 -0500

    Fixed navigation bar to include js file in header layouts

--- public/layouts/admin_header2.php
+++ public/layouts/admin_header2.php
@@ -13,7 +13,7 @@
     <nav class="navbar navbar-default navbar-inverse" role="navigation">
       <div class="container-fluid">
       
-        <!-- Brand and toggle get grouped for better mobile display -->
+        <!-- Brand and toggle get grouped for better mobile display   !-->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
--- public/layouts/header2.php
+++ public/layouts/header2.php
@@ -16,7 +16,7 @@
     <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" id="nav_bar" role="navigation">
       <div class="container-fluid">
       
-        <!-- Brand and toggle get grouped for better mobile display -->
+        <!-- Brand and toggle get grouped for better mobile display  ! -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
